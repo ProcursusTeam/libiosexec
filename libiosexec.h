2be8d140a98f731f6a17f02829591f02dde22dc8
Hayden <pinseeker75@outlook.com>
2021-05-04 10:48:50 -0400

    Formatting libiosexec.h

--- libiosexec.h
+++ libiosexec.h
@@ -1,9 +1,9 @@
-#if defined(__APPLE__)
-#include <TargetConditionals.h>
-
-#if TARGET_OS_IPHONE
 #ifndef LIBIOSEXEC_H
 #define LIBIOSEXEC_H
+
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#if TARGET_OS_IPHONE
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,19 +18,19 @@ int ie_execvp(const char* file, char* const argv[]);
 int ie_execvpe(const char* file, char* const argv[], char* const envp[]);
 int ie_execve(const char* path, char* const argv[], char* const envp[]); 
 
-  #define execl ie_execl
-  #define execle ie_execle
-  #define execlp ie_execlp
-  #define execv ie_execv
-  #define execvp ie_execvp
-  #define execvpe ie_execvpe
-  #define execve ie_execve
+#define execl ie_execl
+#define execle ie_execle
+#define execlp ie_execlp
+#define execv ie_execv
+#define execvp ie_execvp
+#define execvpe ie_execvpe
+#define execve ie_execve
   
 #ifdef __cplusplus
 }
 #endif // __cplusplus
 
-#endif // LIBIOSEXEC_H
-
 #endif // TARGET_IS_IPHONE
 #endif // __APPLE__
+
+#endif // LIBIOSEXEC_H
