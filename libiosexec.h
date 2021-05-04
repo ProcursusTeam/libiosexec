8d2975184cb4754c1a2d5224524ed69e7469859e
Sudhip Nashi <sudhip@nashi.us>
2021-05-04 09:44:56 -0500

    better header

--- libiosexec.h
+++ libiosexec.h
@@ -1,3 +1,7 @@
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+
+#if TARGET_OS_IPHONE
 #ifndef LIBIOSEXEC_H
 #define LIBIOSEXEC_H
 
@@ -14,7 +18,6 @@ int ie_execvp(const char* file, char* const argv[]);
 int ie_execvpe(const char* file, char* const argv[], char* const envp[]);
 int ie_execve(const char* path, char* const argv[], char* const envp[]); 
 
-#ifdef ENABLE_IOSEXEC
   #define execl ie_execl
   #define execle ie_execle
   #define execlp ie_execlp
@@ -22,10 +25,12 @@ int ie_execve(const char* path, char* const argv[], char* const envp[]);
   #define execvp ie_execvp
   #define execvpe ie_execvpe
   #define execve ie_execve
-#endif // ENABLE_IOSEXEC
   
 #ifdef __cplusplus
 }
 #endif // __cplusplus
 
 #endif // LIBIOSEXEC_H
+
+#endif // TARGET_IS_IPHONE
+#endif // __APPLE__
