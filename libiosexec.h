a209596c2eecae93ebbdf5455e7024df4df1fb3b
Hayden <me@diatr.us>
2021-05-04 21:27:36 -0400

    Fix header for nonapple

--- libiosexec.h
+++ libiosexec.h
@@ -1,10 +1,6 @@
 #ifndef LIBIOSEXEC_H
 #define LIBIOSEXEC_H
 
-#if defined(__APPLE__)
-#include <TargetConditionals.h>
-#if TARGET_OS_IPHONE
-
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
@@ -18,21 +14,23 @@ int ie_execvp(const char* file, char* const argv[]);
 int ie_execvpe(const char* file, char* const argv[], char* const envp[]);
 int ie_execve(const char* path, char* const argv[], char* const envp[]); 
 
-#ifndef LIBIOSEXEC_INTERNAL
-    #define execl ie_execl
-    #define execle ie_execle
-    #define execlp ie_execlp
-    #define execv ie_execv
-    #define execvp ie_execvp
-    #define execvpe ie_execvpe
-    #define execve ie_execve
-#endif
+#if defined(__APPLE__)
+#  include <TargetConditionals.h>
+#  if TARGET_OS_IPHONE
+#    ifndef LIBIOSEXEC_INTERNAL
+#      define execl ie_execl
+#      define execle ie_execle
+#      define execlp ie_execlp
+#      define execv ie_execv
+#      define execvp ie_execvp
+#      define execvpe ie_execvpe
+#      define execve ie_execve
+#    endif // LIBIOSEXEC_INTERNAL
+#  endif // TARGET_OS_IPHONE
+#endif // __APPLE__
   
 #ifdef __cplusplus
 }
 #endif // __cplusplus
-
-#endif // TARGET_OS_IPHONE
-#endif // __APPLE__
 
 #endif // LIBIOSEXEC_H
